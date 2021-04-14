f90862fb1bf87d94c1340a8798986b64b19e428d
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-04-14 06:46:50 +0530

    variables in java

---
+++ Java/Variables/Narrowing.java
@@ -0,0 +1,9 @@
+class Narrowing
+{
+	public static void main(String args[])
+	{
+		float a=10.5f;
+		int b=(int)a;
+		System.out.println(b);
+	}
+}
---
+++ Java/Variables/Overflow.java
@@ -0,0 +1,9 @@
+class Overflow
+{
+	public static void main(String args[]){
+		int a=129;
+		byte b=(byte)a;
+	       System.out.println(b);	       
+	}
+
+}
