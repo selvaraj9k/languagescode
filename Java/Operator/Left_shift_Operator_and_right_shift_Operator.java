171eb037b13ab59c4d951ca86b23be6fc99aeb00
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-04-15 21:28:43 +0530

    variables

---
+++ Java/Operator/Left_shift_Operator_and_right_shift_Operator.java
@@ -0,0 +1,8 @@
+class Left_shift_Operator_and_right_shift_Operator
+{
+	public static void main(String[] args)
+	{
+		System.out.println(10<<2);//10*2^2=10*4=40
+		System.out.println(10>>2);//10/2^2=10/4=2
+	}
+}
---
+++ Java/Operator/Shift_Operator.java
@@ -0,0 +1,8 @@
+class Shift_Operator
+{
+	public static void main(String... args)
+	{
+		System.out.println(20>>>2);
+		System.out.println(-20>>>2);
+	}
+}
