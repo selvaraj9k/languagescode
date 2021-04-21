b4600e708c7d98c7f47f9456e5c5c4e0bf18f34c
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-04-21 23:19:13 +0530

    java programs

---
+++ Java/Conditional_Statements/Comments.java
@@ -0,0 +1,12 @@
+public class Comments
+{
+	public static void main(String []args)
+	{
+		//Single line comment
+		int i=9;
+		System.out.println(i);
+		/*multi line
+		 comment*/
+
+	}
+}
---
+++ Java/Conditional_Statements/Document_comment.java
@@ -0,0 +1,8 @@
+public class Document_comment
+{
+	public static void main(String []args)
+	{
+		System.out.println("Linux is the Best Operting System in the world");
+		/** Java Documentation Comment*/
+	}
+}
