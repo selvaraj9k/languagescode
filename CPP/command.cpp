723c61c9870b31182ed889d6066df7be4e51d2b7
selvaraj-kuppusamy <velmanikpvs@gmail.com>
2021-05-23 22:01:44 +0530

    c++ programs

---
+++ CPP/command.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Learn the commnads in c++";
+	//C++ is found in operting system
+	/*C++ is high level language 
+	 * and Object Oriented Programming
+	 */
+}
---
+++ CPP/print.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello linux\n";
+	cout<<"Welcome RedHat!"<<endl;
+	cout<<"Awosome! RedHat";
+	return 0;
+}
